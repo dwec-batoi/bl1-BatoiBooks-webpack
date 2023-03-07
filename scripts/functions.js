7f6ca2b3f9aab66ae3a9b45baf7e01f1748cf5f1
Juan Segura <ja.seguravasco@edu.gva.es>
2023-03-07 19:50:29 +0100

    Enunciado ejercicio 2.3 Arrays

---
+++ scripts/functions.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const NOTES = 'Apunts'
+
+// function booksFromUser(array, number) : array 
+
+// function booksFromModule(array, string) : array 
+
+// function booksCheeperThan(array, number) : array 
+
+// function booksWithStatus(array, string) : array 
+
+// function averagePriceOfBooks(array) : number
+
+// function booksOfTypeNote(array) : array 
+
+// function booksNotOfTypeNote(array) : array 
+
+// function booksNotSold(array) : array 
+
+// function incrementPriceOfbooks(array, number) : undefined
+
+// function getUserById(array, number) : object 
+
+// function getUserIndexById(array, number) : object 
+
+// function getUserByNickName(array, string) : object 
+
+// function getModuleById(array, number) : object 
+
+// function getModuleIndexById(array, number) : object 
+
+module.exports = {
+  booksFromUser,
+  booksFromModule,
+  booksCheeperThan,
+  booksWithStatus,
+  averagePriceOfBooks,
+  booksOfTypeNote,
+  booksNotOfTypeNote,
+  booksNotSold,
+  incrementPriceOfbooks,
+  getUserById,
+  getUserIndexById,
+  getUserByNickName,
+  getModuleById,
+  getModuleIndexById
+}
