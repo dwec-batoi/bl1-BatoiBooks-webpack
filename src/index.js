52812fcc03bf31bdca9fd6a6387ffaeced306c46
Juan Segura <ja.seguravasco@edu.gva.es>
2023-03-20 13:19:33 +0100

    %-DOM sin tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 'use strict'
+
+// Aquí importaremos la clase del controlador e instanciaremos uno
+
+
+// A continuación crearemos una función manejadora para cada formulario
+window.addEventListener('load', () => {
+
+  // función manejadora del formulario 'book-form'
+  document.getElementById('book-form').addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const editorial = document.getElementById('editorial').value
+    
+    // Aquí llamamos al método del controlador para crear un nuevo libro
+    // pasándole los datos del formulario
+  })
+
+  // función manejadora del formulario 'user-form'
+})
